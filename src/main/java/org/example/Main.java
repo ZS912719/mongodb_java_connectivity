7a15199c900711611f7552f04112c4f50f2e4360
phianke <shengxun.zhao@gmail.com>
2025-01-15 16:06:54 +0100

    ping method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.example.dao.MongoConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MongoConnection conn = new MongoConnection();
+        MongoClientSettings settings= conn.getClientSettings();
+
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
